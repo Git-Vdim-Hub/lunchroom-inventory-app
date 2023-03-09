107b1f5f8506c3cb8a313ed44c8fca623773e1de
Git-Vdim-Hub <vadimrace@gmail.com>
2023-03-08 21:53:21 -0500

    set up server.js and connection.js

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const {ApolloServer} = require ('apollo-server-express');
+
+// Import the two parts of a GraphQL schema
+const {typeDefs, resolvers} = require('./schemas');
+const db = require ('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+    await server.start();
+    server.applyMiddleware({app});
+
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API server running on port ${PORT}`);
+            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+        })
+    })
+}
+
+startApolloServer(typeDefs, resolvers);
