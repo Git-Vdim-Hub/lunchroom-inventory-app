28d1f08b5a1eb90b52b768b898244c59ea0cd5bd
Git-Vdim-Hub <vadimrace@gmail.com>
2023-03-09 19:50:16 -0500

    item typedefs and resolvers created

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,13 +9,36 @@ const typeDefs = gql`
         password: String
     }
 
+    type Barcode {
+        _id: ID
+        barcode: String!
+    }
+
+    type Item {
+        _id: ID
+        item_id: Int!
+        item_desc: String!
+        location: String
+        quantity1_name: String!
+        quantity_lvl_1: Int!
+        quantity2_name: String!
+        quantity_lvl_2: Int!
+        quantity3_name: String!
+        quantity_lvl_3: Int!
+        scans: [Barcode]
+    }
+
     type Query {
        users: [User]
-       user(username: String!): User
-       me: User
+       user(id: ID!): User
+       items: [Item]
+       item(itemId: ID!): Item
+       barcodes: [Barcode]
+       barcode(barcodeId: ID!): Barcode
     }
     type Mutation {
         addUser(username: String!, email: String!, password: String!): User
+        addItem(item_id: Int!, item_desc: String!, location: String!, quantity1_name: String!, quantity_lvl_1: Int!, quantity2_name: String!, quantity_lvl_2: Int!, quantity3_name: String!, quantity_lvl_3: Int! barcodeId: ID): Item
     }
 `;
 
