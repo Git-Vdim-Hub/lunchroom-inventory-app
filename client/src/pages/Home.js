64f217d6415085930ff2cc337b85a1e39d26c072
MasakiDesktop <mandij50@gmail.com>
2023-03-11 18:54:25 -0500

    home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -30,24 +30,46 @@ export default function Home() {
 	};
 
 	return (
-		<div>
-			<div>
-				<button onClick={handleBarcodeScanner}>Scan Barcode</button>
+		<div className="flex flex-col mt-16">
+			<div className="flex justify-center w-100 my-3">
+				<div className="flex justify-center border border-2 border-red-400 h-32 w-1/2">
+					<button onClick={handleBarcodeScanner} className="text-xl md:text-2xl">Scan Barcode</button>
+				</div>
 			</div>
-			<p>or</p>
-			<div>
-				<form>
-					<label>Item Number:</label>
-					<input
-						name="itemNumber"
-						type="text"
-						onChange={handleInputChange}
-						value={itemNumber}
-					></input>
-					<button type="button" onClick={handleSubmit}>
-						Search
-					</button>
-				</form>
+			<p className="flex justify-center my-3 text-xl md:text-2xl">or</p>
+			<div className="flex justify-center">
+				<div className="flex justify-center border border-2 border-red-400 h-32 w-1/2 p-3 my-3">
+					<form className="w-100">
+						<label className="grid-span-1">Item Number:</label>
+						<div className="grid-span-1">
+							<input
+								name="itemNumber"
+								type="text"
+								onChange={handleInputChange}
+								value={itemNumber}
+								className="bg-red-300 rounded w-2/3"
+							></input>
+							<button type="button" onClick={handleSubmit}>
+								Search
+							</button>
+						</div>
+					</form>
+					{/* <form className="grid grid-rows-2">
+						<label className="grid-span-1">Item Number:</label>
+						<div className="grid-span-1">
+							<input
+								name="itemNumber"
+								type="text"
+								onChange={handleInputChange}
+								value={itemNumber}
+								className="bg-red-300 rounded w-2/3"
+							></input>
+							<button type="button" onClick={handleSubmit}>
+								Search
+							</button>
+						</div>
+					</form> */}
+				</div>
 			</div>
 		</div>
 	);
