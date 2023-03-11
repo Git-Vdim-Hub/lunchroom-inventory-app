90af744ac95ca19c0233686db797d439d694d840
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-03-11 17:52:03 -0500

    added login to resolvers, added login to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -45,7 +45,7 @@ const typeDefs = gql`
 	}
 
 	type Mutation {
-		login(email: String!, password: String!): Auth
+		login(username: String!, password: String!): Auth
 		addUser(username: String!, email: String!, password: String!): Auth
 		addItem(
 			item_id: String!
