057b6c18c62954e9637f9fc2e86fb2bf2013047c
MasakiDesktop <mandij50@gmail.com>
2023-03-11 19:16:24 -0500

    working on flexing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -39,21 +39,21 @@ export default function Home() {
 			<p className="flex justify-center my-3 text-xl md:text-2xl">or</p>
 			<div className="flex justify-center">
 				<div className="flex justify-center border border-2 border-red-400 h-32 w-1/2 p-3 my-3">
-					<form className="w-100">
-						<label className="grid-span-1">Item Number:</label>
-						<div className="grid-span-1">
+					<div className="w-full flex relative justify-center">
+						<label className=" absolute md:text-lg lg:text-xl top-0 left-0">Item Number:</label>
+						<div className="self-center w-full">
 							<input
 								name="itemNumber"
 								type="text"
 								onChange={handleInputChange}
 								value={itemNumber}
-								className="bg-red-300 rounded w-2/3"
+								className="bg-red-300 rounded w-5/6 md:w-3/5 mr-1 md:mr-2"
 							></input>
 							<button type="button" onClick={handleSubmit}>
 								Search
 							</button>
 						</div>
-					</form>
+					</div>
 					{/* <form className="grid grid-rows-2">
 						<label className="grid-span-1">Item Number:</label>
 						<div className="grid-span-1">
