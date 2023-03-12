e971a0d3819e6c8cc070ec9ce44e673261497875
MasakiDesktop <mandij50@gmail.com>
2023-03-11 21:39:29 -0500

    theme changing

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
 	return (
 		<div>
-			<div className="navbar bg-neutral flex justify-start px-6 items-center h-16">
+			<div className="navbar bg-primary flex justify-start px-6 items-center h-16">
 				<p className="text-xl text-white">Lunchroom Inventory</p>
 				{/* <Link to={"/"} onClick={() => console.log("Takes you to Home")}>Home</Link>
 				<Link to={"./Inventory"} onClick={() => console.log("Takes you to Inventory Page")}>Inventory</Link>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -32,24 +32,24 @@ export default function Home() {
 	return (
 		<div className="flex flex-col mt-16">
 			<div className="flex justify-center w-100 my-3">
-				<div className="flex justify-center border border-2 border-red-400 h-32 w-4/5 lg:w-2/3">
-					<button onClick={handleBarcodeScanner} className="text-xl md:text-2xl">Scan Barcode</button>
+				<div className="flex justify-center bg-neutral border border-8 border-neutral-content h-32 w-4/5 lg:w-2/3">
+					<button onClick={handleBarcodeScanner} className="text-xl text-base-100 md:text-2xl">Scan Barcode</button>
 				</div>
 			</div>
-			<p className="flex justify-center my-3 text-xl md:text-2xl">or</p>
+			<p className="flex justify-center text-neutral my-3 text-xl md:text-2xl">or</p>
 			<div className="flex justify-center">
-				<div className="flex justify-center border border-2 border-red-400 h-32 w-4/5 lg:w-2/3 p-3 my-3">
+				<div className="flex justify-center bg-neutral border border-8 border-neutral-content h-32 w-4/5 lg:w-2/3 p-3 my-3">
 					<div className="w-full flex relative justify-center">
-						<label className=" absolute md:text-lg lg:text-xl top-0 left-0">Item Number:</label>
+						<label className=" absolute text-base-100 md:text-lg lg:text-xl top-0 left-0">Item Number:</label>
 						<div className="self-center w-full mt-4">
 							<input
 								name="itemNumber"
 								type="text"
 								onChange={handleInputChange}
 								value={itemNumber}
-								className="bg-red-400 rounded w-5/6 md:w-3/5 mr-1 md:mr-2"
+								className="bg-neutral-base-100 rounded w-5/6 md:w-3/5 mr-1 md:mr-2"
 							></input>
-							<button type="button" onClick={handleSubmit}>
+							<button className="btn border-2 border-neutral-content" type="button" onClick={handleSubmit}>
 								Search
 							</button>
 						</div>
