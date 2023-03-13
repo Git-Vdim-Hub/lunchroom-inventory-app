d515aaf6540842f09eda24e40e7f5f68ebcd6319
MasakiDesktop <mandij50@gmail.com>
2023-03-13 14:41:17 -0400

    item pages styling

--- client/src/pages/Item.js
+++ client/src/pages/Item.js
@@ -12,28 +12,28 @@ export default function Item() {
         <div className="flex justify-center gap-10 mt-14">
             <div className="flex flex-col">
                 <label className="label">Item ID:</label>
-                <input className="input input-bordered border-2 border-secondary mb-4 hover:border-secondary-focus" value={test} onChange={handleChange}/>
+                <input className="input input-bordered border-2 border-warning mb-4 hover:border-accent" value={test} onChange={handleChange}/>
                 <label>Description:</label>
-                <input className="input input-bordered border-2 mb-4 h-28" type="text"/>
+                <input className="input input-bordered border-2 border-warning mb-4 h-28 hover:border-accent" type="text"/>
                 <label>Location:</label>
-                <input className="input input-bordered border-2 mb-4" type="text"/>
+                <input className="input input-bordered border-2 border-warning mb-4 hover:border-accent" type="text"/>
                 <div className="mt-6">
                     <p>Barcodes?</p>
                 </div>
             </div>
             <div className="flex flex-col">
                 <label>Level 1 Name:</label>
-                <input className="border border-2 border-zinc-600 rounded mb-4 w-16" type="text"/>
+                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="text"/>
                 <label>Level 1 Quantity:</label>
-                <input className="border border-2 border-zinc-600 rounded mb-4 w-16" type="number"/>
+                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="number"/>
                 <label>Level 2 Name:</label>
-                <input className="border border-2 border-zinc-600 rounded mb-4 w-16" type="text"/>
+                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="text"/>
                 <label>Level 2 Quantity:</label>
-                <input className="border border-2 border-zinc-600 rounded mb-4 w-16" type="number"/>
+                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="number"/>
                 <label>Level 3 Name:</label>
-                <input className="border border-2 border-zinc-600 rounded mb-4 w-16" type="text"/>
+                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="text"/>
                 <label>Level 3 Quantity:</label>
-                <input className="border border-2 border-zinc-600 rounded mb-4 w-16" type="number"/>
+                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="number"/>
             </div>
         </div>
     )
