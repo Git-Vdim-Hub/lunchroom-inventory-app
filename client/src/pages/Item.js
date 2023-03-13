6441a9e6e1851936c5e6b2573cd94591b5b21fb4
MasakiDesktop <mandij50@gmail.com>
2023-03-13 15:39:10 -0400

    changed item page boxes, text area for desc

--- client/src/pages/Item.js
+++ client/src/pages/Item.js
@@ -9,31 +9,44 @@ export default function Item() {
     }
 
     return(
-        <div className="flex justify-center gap-10 mt-14">
-            <div className="flex flex-col">
-                <label className="label">Item ID:</label>
+        <div className="flex flex-col md:flex-row justify-center gap-10 mt-14 m-8">
+            <div className="flex flex-col justify-center w-72">
+                <label>Item ID:</label>
                 <input className="input input-bordered border-2 border-warning mb-4 hover:border-accent" value={test} onChange={handleChange}/>
                 <label>Description:</label>
-                <input className="input input-bordered border-2 border-warning mb-4 h-28 hover:border-accent" type="text"/>
+                <textarea className="input input-bordered border-2 border-warning mb-4 h-28 hover:border-accent" />
+                {/* <input className="input input-bordered border-2 border-warning mb-4 h-28 flex justify-start hover:border-accent" type="text"/> */}
                 <label>Location:</label>
                 <input className="input input-bordered border-2 border-warning mb-4 hover:border-accent" type="text"/>
                 <div className="mt-6">
                     <p>Barcodes?</p>
                 </div>
             </div>
-            <div className="flex flex-col">
-                <label>Level 1 Name:</label>
-                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="text"/>
-                <label>Level 1 Quantity:</label>
-                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="number"/>
-                <label>Level 2 Name:</label>
-                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="text"/>
-                <label>Level 2 Quantity:</label>
-                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="number"/>
-                <label>Level 3 Name:</label>
-                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="text"/>
-                <label>Level 3 Quantity:</label>
-                <input className="input input-bordered border-2 border-warning mb-4 w-16 hover:border-accent" type="number"/>
+            <div className="grid grid-cols-2 justify-items-center md:gap-5">
+                <div className="flex flex-col">
+                    <label>Level 1 Name:</label>
+                    <input className="input input-bordered border-2 border-warning mb-4 w-32 hover:border-accent" type="text"/>
+                </div>
+                <div className="flex flex-col">
+                    <label>Level 1 Quantity:</label>
+                    <input className="input input-bordered border-2 border-warning mb-4 w-32 hover:border-accent" type="number"/>
+                </div>
+                <div className="flex flex-col">
+                    <label>Level 2 Name:</label>
+                    <input className="input input-bordered border-2 border-warning mb-4 w-32 hover:border-accent" type="text"/>
+                </div>
+                <div className="flex flex-col">
+                    <label>Level 2 Quantity:</label>
+                    <input className="input input-bordered border-2 border-warning mb-4 w-32 hover:border-accent" type="number"/>
+                </div>
+                <div className="flex flex-col">
+                    <label>Level 3 Name:</label>
+                    <input className="input input-bordered border-2 border-warning mb-4 w-32 hover:border-accent" type="text"/>
+                </div>
+                <div className="flex flex-col">
+                    <label>Level 3 Quantity:</label>
+                    <input className="input input-bordered border-2 border-warning mb-4 w-32 hover:border-accent" type="number"/>
+                </div>
             </div>
         </div>
     )
