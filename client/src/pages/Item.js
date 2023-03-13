e2232fc131c70fc6bb9d7af7bfddd637464bd478
MasakiDesktop <mandij50@gmail.com>
2023-03-13 17:00:41 -0400

    changed the btns on item page

--- client/src/pages/Item.js
+++ client/src/pages/Item.js
@@ -32,48 +32,50 @@ export default function Item() {
     }
     console.log("this is the local state right now", itemState)
     return(
-        <div className="flex flex-col md:flex-row justify-center gap-10 mt-14 m-8">
-            <div className="flex flex-col justify-center w-72">
-                <label>Item ID:</label>
-                <input className="input input-bordered border-2 border-primary mb-4 hover:border-primary-focus"  value={itemState?.item_id} onChange={handleChange} placeholder={item.item_id}/>
-                <label>Description:</label>
-                <textarea className="input input-bordered border-2 border-primary mb-4 h-28 hover:border-primary-focus" value={itemState?.item_desc} type="text" onChange={handleChange} placeholder={item.item_desc}/>
-                <label>Location:</label>
-                <input className="input input-bordered border-2 border-primary mb-4 hover:border-primary-focus" value={itemState?.location} onChange={handleChange} placeholder={item.location} type="text"/>
-                <div className="mt-6">
-                    <p>Barcodes?</p>
+        <div>
+            <div className="flex flex-col md:flex-row justify-center gap-10 mt-14 m-8">
+                <div className="flex flex-col justify-center w-72">
+                    <label>Item ID:</label>
+                    <input className="input input-bordered border-2 border-primary mb-4 hover:border-primary-focus"  value={itemState?.item_id} onChange={handleChange} placeholder={item.item_id}/>
+                    <label>Description:</label>
+                    <textarea className="input input-bordered border-2 border-primary mb-4 h-28 hover:border-primary-focus" value={itemState?.item_desc} type="text" onChange={handleChange} placeholder={item.item_desc}/>
+                    <label>Location:</label>
+                    <input className="input input-bordered border-2 border-primary mb-4 hover:border-primary-focus" value={itemState?.location} onChange={handleChange} placeholder={item.location} type="text"/>
+                    <div className="mt-6">
+                        <p>Barcodes?</p>
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 justify-items-center md:gap-5">
+                    <div className="flex flex-col">
+                        <label>Level 1 Name:</label>
+                        <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity1_name} type="text" onChange={handleChange} placeholder={item.quantity1_name}/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label>Level 1 Quantity:</label>
+                        <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus"  value={itemState?.quantity_lvl_1} type="number" onChange={handleChange} placeholder={item.quantity_lvl_1}/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label>Level 2 Name:</label>
+                        <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity2_name} type="text" onChange={handleChange} placeholder={item.quantity2_name}/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label>Level 2 Quantity:</label>
+                        <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity_lvl_2} type="number" onChange={handleChange} placeholder={item.quantity_lvl_2}/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label>Level 3 Name:</label>
+                        <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity3_name} type="text" onChange={handleChange} placeholder={item.quantity3_name}/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label>Level 3 Quantity:</label>
+                        <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity_lvl_3} type="number" onChange={handleChange} placeholder={item.quantity_lvl_3}/>
+                    </div>
                 </div>
             </div>
-            <div className="grid grid-cols-2 justify-items-center md:gap-5">
-                <div className="flex flex-col">
-                    <label>Level 1 Name:</label>
-                    <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity1_name} type="text" onChange={handleChange} placeholder={item.quantity1_name}/>
-                </div>
-                <div className="flex flex-col">
-                    <label>Level 1 Quantity:</label>
-                    <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus"  value={itemState?.quantity_lvl_1} type="number" onChange={handleChange} placeholder={item.quantity_lvl_1}/>
-                </div>
-                <div className="flex flex-col">
-                    <label>Level 2 Name:</label>
-                    <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity2_name} type="text" onChange={handleChange} placeholder={item.quantity2_name}/>
-                </div>
-                <div className="flex flex-col">
-                    <label>Level 2 Quantity:</label>
-                    <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity_lvl_2} type="number" onChange={handleChange} placeholder={item.quantity_lvl_2}/>
-                </div>
-                <div className="flex flex-col">
-                    <label>Level 3 Name:</label>
-                    <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity3_name} type="text" onChange={handleChange} placeholder={item.quantity3_name}/>
-                </div>
-                <div className="flex flex-col">
-                    <label>Level 3 Quantity:</label>
-                    <input className="input input-bordered border-2 border-primary mb-4 w-32 hover:border-primary-focus" value={itemState?.quantity_lvl_3} type="number" onChange={handleChange} placeholder={item.quantity_lvl_3}/>
-                </div>
-            </div>
-            <div>
-                <button>Update</button>
-                <button>Delete</button>
-                <button>Cancel</button>
+            <div className="flex justify-center gap-5">
+                <button className="btn md:w-36">Update</button>
+                <button className="btn md:w-36">Delete</button>
+                <button className="btn md:w-36">Cancel</button>
             </div>
         </div>
     )
