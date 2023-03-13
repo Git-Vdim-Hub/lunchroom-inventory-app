f0463fc43a67e1505739b3d7219d2512c36474f6
MasakiDesktop <mandij50@gmail.com>
2023-03-13 13:06:52 -0400

    added button on nav or loggin out, it's commented out

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
 	return (
 		<div>
-			<div className="navbar bg-primary flex justify-start px-6 items-center h-16">
+			<div className="navbar bg-primary flex justify-between px-6 items-center h-16">
 				<p className="text-xl text-white">Lunchroom Inventory</p>
+				{/* <button className="btn btn-sm">Log Out</button> */}
 			</div>
 		</div>
 	);
