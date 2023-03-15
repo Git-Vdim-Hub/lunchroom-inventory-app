2bf870d35054dfd9044fdda53da66f0f2c73f350
MasakiDesktop <mandij50@gmail.com>
2023-03-15 11:46:05 -0400

    inventory cards

--- client/src/components/ItemList/index.js
+++ client/src/components/ItemList/index.js
@@ -6,7 +6,7 @@ const ItemList = ({items}) => {
         return <h3>No Items Yet</h3>
     }
     return (
-        <div className="">
+        <div className="lg:flex lg:flex-wrap lg:justify-around">
             {/* <div className="grid grid-row-2 border-2 border-primary rounded">
                 <div className="row-span-1 flex">
                     <select className="select select-xs select-primary w-36">
@@ -30,13 +30,31 @@ const ItemList = ({items}) => {
                 </div>
             </div> */}
             {items.map((item) =>(
-                <div className="border-2 border-primary rounded flex p-5" 
-                    key={item._id}>Bin: {item.location} Id: {item.item_id} Description: {item.item_desc} Name 1: {item.quantity1_name} Qty 1: {item.quantity_lvl_1} Name 2: {item.quantity2_name} Qty 2: {item.quantity_lvl_2} Name 3: {item.quantity3_name} Qty 3: {item.quantity_lvl_3}
-                <Link
-                to={`/Item/${item._id}`}
-                >Edit</Link>
-                <button>Verify</button>
+                <div className="card bg-base-200 shadow-lg border-2 border-primary mt-5 lg:w-96">
+                    <div className="card-body">
+                        <h2 className="card-title">Bin: {item.location} Id: {item.item_id}</h2>
+                        <p><b>Description:</b> {item.item_desc}</p>
+                        <div className="grid grid-cols-2">
+                            <p><b>Name 1:</b> {item.quantity1_name}</p>
+                            <p><b>Qty 1:</b> {item.quantity_lvl_1}</p>
+                            <p><b>Name 2:</b> {item.quantity2_name}</p>
+                            <p><b>Qty 2:</b> {item.quantity_lvl_2}</p>
+                            <p><b>Name 3:</b> {item.quantity3_name}</p>
+                            <p><b>Qty 3:</b> {item.quantity_lvl_3}</p>
+                        </div>
+                        <div className="card-actions justify-end mt-2">
+                            <Link to={`/Item/${item._id}`} className="btn btn-sm md:btn-md btn-primary">Edit</Link>
+                            <button className="btn btn-sm md:btn-md btn-primary">Verify</button>
+                        </div>
+                    </div>
                 </div>
+                // <div className="border-2 border-primary rounded flex p-5 mt-4" 
+                //     key={item._id}>Bin: {item.location} Id: {item.item_id} Description: {item.item_desc} Name 1: {item.quantity1_name} Qty 1: {item.quantity_lvl_1} Name 2: {item.quantity2_name} Qty 2: {item.quantity_lvl_2} Name 3: {item.quantity3_name} Qty 3: {item.quantity_lvl_3}
+                // <Link
+                // to={`/Item/${item._id}`}
+                // >Edit</Link>
+                // <button>Verify</button>
+                // </div>
             ))}
         </div>
     )
