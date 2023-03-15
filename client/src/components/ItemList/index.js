a151a101092901d67fdfa8365670ef69f6e402ee
MasakiDesktop <mandij50@gmail.com>
2023-03-15 13:46:14 -0400

    Added key thing

--- client/src/components/ItemList/index.js
+++ client/src/components/ItemList/index.js
@@ -7,30 +7,8 @@ const ItemList = ({items}) => {
     }
     return (
         <div className="lg:flex lg:flex-wrap lg:justify-around mt-40">
-            {/* <div className="grid grid-row-2 border-2 border-primary rounded">
-                <div className="row-span-1 flex">
-                    <select className="select select-xs select-primary w-36">
-                        <option disabled selected>Bin</option>
-                        <option>Freezer</option>
-                        <option>Cooler</option>
-                        <option>Pantry</option>
-                    </select>
-                    <div className="flex justify-around w-full">
-                        <h2 className="text-sm">Id</h2>
-                        <h2 className="text-sm">Desc</h2>
-                    </div>
-                </div>
-                <div className='row-span-1 flex justify-around'>
-                    <h2 className="text-sm">Lvl 1 Name</h2>
-                    <h2 className="text-sm">Qty</h2>
-                    <h2 className="text-sm">Lvl 2 Name</h2>
-                    <h2 className="text-sm">Qty</h2>
-                    <h2 className="text-sm">Lvl 3 Name</h2>
-                    <h2 className="text-sm">Qty</h2>
-                </div>
-            </div> */}
             {items.map((item) =>(
-                <div className="card bg-base-200 shadow-lg border-2 border-primary mt-5 lg:w-96">
+                <div className="card bg-base-200 shadow-lg border-2 border-primary mt-5 lg:w-96" key={item._id}>
                     <div className="card-body p-3">
                         <h2 className="card-title">Bin: {item.location} Id: {item.item_id}</h2>
                         <p><b>Description:</b> {item.item_desc}</p>
@@ -47,13 +25,6 @@ const ItemList = ({items}) => {
                         </div>
                     </div>
                 </div>
-                // <div className="border-2 border-primary rounded flex p-5 mt-4" 
-                //     key={item._id}>Bin: {item.location} Id: {item.item_id} Description: {item.item_desc} Name 1: {item.quantity1_name} Qty 1: {item.quantity_lvl_1} Name 2: {item.quantity2_name} Qty 2: {item.quantity_lvl_2} Name 3: {item.quantity3_name} Qty 3: {item.quantity_lvl_3}
-                // <Link
-                // to={`/Item/${item._id}`}
-                // >Edit</Link>
-                // <button>Verify</button>
-                // </div>
             ))}
         </div>
     )
